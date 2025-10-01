e296ee4e01e8def446164104a1b5fbbe3371cf7e
felipectm <felipe_ctm@hotmail.com>
2025-10-01 20:52:27 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ bq query --use_legacy_sql=false \
 CREATE OR REPLACE EXTERNAL TABLE `thelook_gcda.product_returns`
 OPTIONS (
 format ="PARQUET",
-uris = ['gs://sureskills-lab-dev/DAC2M2L4/returns/returns_*.parquet']
+uris = 'gs://sureskills-lab-dev/DAC2M2L4/returns/returns_*.parquet'
 );
 '
 
